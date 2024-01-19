99b5f87a0c5081491a8bbbb2d3f55987d3189b0c
airRnot1106 <yngw.rintaro@gmail.com>
2024-01-19 14:44:35 +0900

    feat: Add constants, childProcess, python files

---
+++ src/main/libs/childProcess/index.ts
@@ -0,0 +1,56 @@
+import { exec } from 'child_process';
+import { Result } from '@src/types/result';
+
+export const isJSONString = (str: string): boolean => {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const execAsync = async <T = string>(
+  command: string,
+  args: string[],
+): Promise<Result<{ stdout: T; stderr: string }, Error>> => {
+  const [normalisedCommand, ...normalisedArgs] = [command, ...args].map(
+    (str) => {
+      let s = str;
+      s = s.replace(/\//g, '\\');
+      s = s.replace(/"/, `\"`);
+      s = s.replace(/\|/, '^|');
+      s = s.replace(/</, '^<');
+      s = s.replace(/>/, '^>');
+      s = s.replace(/&/, '^&');
+      s = s.replace(/;/, '^;');
+      s = s.replace(/%/, '^%');
+      s = s.replace(/!/, '^!');
+      s = s.replace(/\^/, '^^');
+      return `"${s}"`;
+    },
+  );
+
+  return new Promise((resolve, reject) => {
+    exec(
+      `${normalisedCommand} ${normalisedArgs.join(' ')}`,
+      (err, stdout, stderr) => {
+        if (err) {
+          resolve({
+            ok: false,
+            error: new Error(`Exception: ${JSON.stringify({ stderr, err })}`),
+          });
+        } else {
+          const isJSON = isJSONString(stdout);
+          if (isJSON) {
+            const jsonStd = JSON.parse(stdout) as T;
+            resolve({ ok: true, value: { stdout: jsonStd, stderr } });
+          } else {
+            const std = stdout as T;
+            resolve({ ok: true, value: { stdout: std, stderr } });
+          }
+        }
+      },
+    );
+  });
+};
---
+++ src/main/python/index.ts
@@ -0,0 +1,1 @@
+export { initialize } from '@main/python/initializer';
